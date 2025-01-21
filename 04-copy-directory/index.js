2e5fbdbddcdfea119e5757e3d6cbf14a132fe10a
bartosh93 <bartosh93@yandex.ru>
2025-01-22 00:31:28 +0300

    feat: resolve copy directory subtask

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const dir = '04-copy-directory';
+const sourceDir = path.join(dir, 'files');
+const targetDir = path.join(dir, 'new-folder');
+
+async function copyFiles() {
+  try {
+    await fs.mkdir(targetDir, { recursive: true });
+
+    const files = await fs.readdir(sourceDir);
+
+    for (const file of files) {
+      const srcFilePath = path.join(sourceDir, file);
+      const destFilePath = path.join(targetDir, file);
+      await fs.copyFile(srcFilePath, destFilePath);
+    }
+  } catch (error) {
+    console.error('Произошла ошибка:', error);
+  }
+}
+copyFiles();
